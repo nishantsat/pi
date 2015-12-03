4243dbe1258b67d7d74e66597ce7a08392c34608
Nishant S <nishantsat@users.noreply.github.com>
2015-12-03 22:36:20 +0800

    ui changes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSplitter>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -7,12 +10,26 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->mainToolBar->addAction("Run");
-    ui->tvNavBar->setMinimumWidth(100);
-    ui->tvNavBar->setMaximumWidth(200);
 
+    // Add the input and output screens
+    QSplitter* splitterA = new QSplitter();
+    splitterA->setOrientation(Qt::Vertical);
+    splitterA->addWidget(ui->txtInput);
+    splitterA->addWidget(ui->txtOutput);
     ui->txtOutput->setStyleSheet("QTextEdit {background-color: black;}");
     ui->txtOutput->setTextColor(QColor("white"));
-    ui->txtOutput->setText("Hello");
+    ui->txtOutput->setText("[Ready]");
+
+    QSplitter* splitterB = new QSplitter();
+    splitterB->addWidget(ui->tvNavBar);
+    splitterB->addWidget(splitterA);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(splitterB);
+
+    QWidget *window = new QWidget();
+    window->setLayout(layout);
+    setCentralWidget(window);
 }
 
 MainWindow::~MainWindow()
