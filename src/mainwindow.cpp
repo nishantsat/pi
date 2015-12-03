365e8b8b5ffd32229902f57a298cecf10b4e4e9d
Nishant S <nishantsat@users.noreply.github.com>
2015-12-03 21:48:18 +0800

    ui changes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,6 +7,12 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->mainToolBar->addAction("Run");
+    ui->tvNavBar->setMinimumWidth(100);
+    ui->tvNavBar->setMaximumWidth(200);
+
+    ui->txtOutput->setStyleSheet("QTextEdit {background-color: black;}");
+    ui->txtOutput->setTextColor(QColor("white"));
+    ui->txtOutput->setText("Hello");
 }
 
 MainWindow::~MainWindow()
